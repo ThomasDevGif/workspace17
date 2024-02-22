7c6140efb4ef4bf2b53f135a67d9b4e5d1186b75
Thomas Tournoux <thomasdev.gif@gmail.com>
2024-02-22 10:36:15 +0100

    initial commit

---
+++ projects/my-lib/src/lib/my-lib.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-my-lib',
+  template: `
+    <p>
+      my-lib works!
+    </p>
+  `,
+  styleUrls: [
+    './my-lib.component.scss'
+  ]
+})
+export class MyLibComponent {
+
+}
